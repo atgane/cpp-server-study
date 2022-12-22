251bb506fff1f2e265628b8d2e6e6f5277303135
atgane <hyper2012@naver.com>
2022-12-22 23:37:44 +0900

    Feat: basic network

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -9,58 +9,73 @@
 #include <vector>
 #include <thread>
 
-#include "ThreadManager.h"
-#include "RefCounting.h"
-#include "memory.h"
-#include "Allocator.h"
-#include "TypeCast.h"
+#include <WinSock2.h>
+#include <mswsock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
-class Class
+int main()
 {
-public:
-	virtual void Print() {
-
+	// 1. 소켓 라이브러리 초기화
+// 관련 정보가 wsaData에 채워진다. 
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		return 0;
 	}
 
-private:
-	int _hp = 0;
-	int _mp = 0;
-};
+	// 2. 소켓 열기
+	// AF_INET: IPv4, AF_INET6: IPv6
+	// SOCK_STREAM: tcp, SOCK_DGRAM: udp
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket Error code: " << errCode << endl;
+		return 0;
+	}
 
-class Knight : public Class
-{
-private:
-	int _hp = 0;
-	int _mp = 0;
-};
+	// 3. 주소만들기
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	serverAddr.sin_port = ::htons(7777);
 
-class Archer : public Class
-{
-private:
-	int _hp = 0;
-	int _mp = 0;
-};
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind Error code: " << errCode << endl;
+		return 0;
+	}
 
-class Gunner : public Class
-{
-private:
-	int _hp = 0;
-	int _mp = 0;
-};
+	// 4. 영업 시작
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen Error code: " << errCode << endl;
+		return 0;
+	}
 
-class Mage : public Class
-{
-private:
-	int _hp = 0;
-	int _mp = 0;
-};
+	while (true)
+	{
+		SOCKADDR_IN clientAddr;
+		::memset(&clientAddr, 0, sizeof(clientAddr));
 
-int main() {
-	using TL = TypeList<Class, Knight, Archer, Gunner, Mage>;
-	
-	TypeConversion<TL> test;
+		int32 addrLen = sizeof(clientAddr);
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept Error code: " << errCode << endl;
+			return 0;
+		}
 
-	test.s_convert[0][0];
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client IP = " << ipAddress << endl;
+	}
 
-	cout << endl;
+	// 7. 접속 종료
+	::WSACleanup();
 }
